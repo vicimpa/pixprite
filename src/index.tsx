4be0bfc74c6aa543760b7a8dedeb89ff6a4e27cd
PromiSe#### <vicimpa@ya.ru>
2025-09-05 00:26:55 +0300

    Remove context-menu

--- src/index.tsx
+++ src/index.tsx
@@ -2,5 +2,9 @@ import { createRoot } from "react-dom/client";
 import { byId } from "./utils/dom";
 import { App } from "./app";
 
+addEventListener('contextmenu', e => {
+  e.preventDefault();
+});
+
 createRoot(byId('root', true))
   .render(<App />);
